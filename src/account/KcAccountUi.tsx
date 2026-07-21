97a30212bfd3eb2794e224311d255e94e700fb10
Jakub Recman <jakub@oxidian.com>
2026-07-21 21:46:42 +0000

    Add EODH-branded Single-Page account theme

---
+++ src/account/KcAccountUi.tsx
@@ -0,0 +1,41 @@
+/**
+ * This file has been claimed for ownership from @keycloakify/keycloak-account-ui version 260700.0.0.
+ * To relinquish ownership and restore this file to its original content, run the following command:
+ *
+ * $ npx keycloakify own --path "account/KcAccountUi.tsx" --revert
+ */
+
+import "@patternfly/patternfly/patternfly-addons.css";
+import "@patternfly/react-core/dist/styles/base.css";
+import "./main.css";
+
+import { useReducer, useEffect } from "react";
+import { KeycloakProvider } from "../shared/keycloak-ui-shared";
+import { environment } from "./environment";
+import { i18n } from "./i18n/i18n";
+import { Root } from "./root/Root";
+import { SessionExpirationWarningOverlay } from "../shared/SessionExpirationWarningOverlay";
+
+document.title = "Account Management";
+
+const prI18nInitialized = i18n.init();
+// EODH: dark mode intentionally off (startColorSchemeManagement would re-enable it)
+
+export default function KcAccountUi() {
+    const [isI18nInitialized, setI18nInitialized] = useReducer(() => true, false);
+
+    useEffect(() => {
+        prI18nInitialized.then(() => setI18nInitialized());
+    }, []);
+
+    if (!isI18nInitialized) {
+        return null;
+    }
+
+    return (
+        <KeycloakProvider environment={environment}>
+            <Root />
+            <SessionExpirationWarningOverlay warnUserSecondsBeforeAutoLogout={45} />
+        </KeycloakProvider>
+    );
+}
